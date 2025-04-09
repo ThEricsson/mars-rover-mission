906fa9367cf2b44b968b5ddcb7e99fad116afb23
ThEricsson <soc.l.eric@icloud.com>
2025-04-09 20:36:30 +0200

    ✨feat: update ControlerView layout with improved subcard styling and enhanced typography

--- src/views/rover/ControlerView.vue
+++ src/views/rover/ControlerView.vue
@@ -11,11 +11,11 @@
     <div class="card h-99 m-6">
       <h1 class="text-center text-4xl mb-4">Rover control center</h1>
       <div class="grid grid-cols-2 gap-4">
-        <div class="card bg-white-100 shadow-xl">
-          <h2 class="text-xl text-center">Controler</h2>
+        <div class="subcard bg-white-100 shadow-xl">
+          <h2 class="text-3xl text-center">Controler</h2>
         </div>
-        <div class="card bg-base-100 shadow-xl">
-          <h2 class="text-xl text-center">Status history</h2>
+        <div class="subcard bg-base-100 shadow-xl">
+          <h2 class="text-3xl text-center">Status history</h2>
         </div>
       </div>
     </div>
