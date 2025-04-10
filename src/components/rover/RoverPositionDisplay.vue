bc37607398857896b9f906f3294bb0c6a750cf27
ThEricsson <soc.l.eric@icloud.com>
2025-04-10 22:51:06 +0200

    ⚙️wip: refactor RoverPositionDisplay to enhance layout and styling for position and direction display

--- src/components/rover/RoverPositionDisplay.vue
+++ src/components/rover/RoverPositionDisplay.vue
@@ -11,13 +11,22 @@ const roverStore = useRoverStore()
 <template>
   <div class="grid grid-cols-3 gap-4">
     <div class="content text-center">
-      <p class="text-2xl">X → {{ roverStore.getRoverPosition().value.x }}</p>
+      <p class="text-2xl">
+        <span class="font-bold">X </span>
+        <span class="font-bold text-red-500">{{ roverStore.getRoverPosition().value.x }}</span>
+      </p>
     </div>
     <div class="content text-center">
-      <p class="text-2xl">Y → {{ roverStore.getRoverPosition().value.y }}</p>
+      <p class="text-2xl">
+        <span class="font-bold">Y </span>
+        <span class="font-bold text-blue-500">{{ roverStore.getRoverPosition().value.y }}</span>
+      </p>
     </div>
     <div class="content text-center">
-      <p class="text-2xl">Direction → {{ roverStore.getRoverDirection().value }}</p>
+      <p class="text-2xl">
+        <span class="font-bold">CP </span>
+        <span class="font-bold text-emerald-500">{{ roverStore.getRoverDirection().value }}</span>
+      </p>
     </div>
   </div>
 </template>
